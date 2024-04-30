715646567e90c98bab7a87466157085f1186be65
thereallucas98 <lucas1998david@gmail.com>
2024-04-30 08:50:27 -0300

    chore: api initial configuration && permission

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { ability } from '@acl/auth'
+
+const userCanInviteSomeoneElese = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElese)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)

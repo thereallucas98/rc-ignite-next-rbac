da0857a80c0e042f7e0824f8e848625426697d3c
thereallucas98 <lucas1998david@gmail.com>
2024-06-03 20:14:38 -0300

    feat: accept invite

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ This project contains all the necessary boilerplate to setup a multi-tenant SaaS
 ### Invites
 
 - [X] It should be able to invite a new member (e-mail, role);
-- [ ] It should be able to accept an invite;
+- [X] It should be able to accept an invite;
 - [ ] It should be able to revoke a pending invite;
 
 ### Members
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recovery'
 import { resetPassword } from './routes/auth/reset-password'
+import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invites'
 import { getMembers } from './routes/members/get-members'
@@ -105,6 +106,7 @@ app.register(removeMember)
 // INVITES
 app.register(createInvite)
 app.register(getInvite)
+app.register(acceptInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')

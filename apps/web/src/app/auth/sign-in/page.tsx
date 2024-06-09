a9c5cb90863a4a2a9b8fc6e9c8bc63a715a01aab
thereallucas98 <lucas1998david@gmail.com>
2024-06-09 10:27:22 -0300

    feat: using server actions to login with password

--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -7,9 +7,11 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
+import { signInWithEmailAndPassword } from './actions'
+
 export default function SignInPage() {
   return (
-    <form action="" className="space-y-4">
+    <form action={signInWithEmailAndPassword} className="space-y-4">
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
@@ -27,8 +29,12 @@ export default function SignInPage() {
         </Link>
       </div>
 
-      <Button className="w-full" type="submit" variant="link">
-        <Link href="/auth/sign-up">Sign in with e-mail</Link>
+      <Button className="w-full" type="submit">
+        Sign in with e-mail
+      </Button>
+
+      <Button className="w-full" variant="link" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
       </Button>
 
       <Separator />
